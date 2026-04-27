8567b63f30fb38923fe550595d4a5465d23bb799
paglia <paglieranipietro@gmail.com>
2026-04-27 10:50:40 +0200

    modifiche, ancora da finire

--- index.php
+++ index.php
@@ -4,58 +4,62 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use DI\Container as Container;
-require 'Middleware/JwtMiddleware.php';
-use Middleware\JwtMiddleware;
 
-// Autoload
 require '../vendor/autoload.php';
 
-// Model & Util
 require 'Util/Connection.php';
 require 'Model/UtenteRepository.php';
 require 'Model/ParkingLotRepository.php';
 require 'Model/ReservationRepository.php';
 
-// Controller
 require 'Controller/AuthController.php';
 require 'Controller/ParkingLotController.php';
 require 'Controller/ReservationController.php';
 
+require 'Middleware/JwtMiddleware.php';
+
 use Controller\AuthController;
 use Controller\ParkingLotController;
 use Controller\ReservationController;
+use Middleware\JwtMiddleware;
 
-// Configurazione
 $config = require 'conf/config.php';
 $container = new Container();
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-$app->setBasePath($config['BASEPATH']);
+$basePath = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
+$app->setBasePath($basePath);
+
 $app->addBodyParsingMiddleware();
 $container->set('config', $config);
 
-
-// ROTTE CHE CHIUNQUE PUÒ USARE (senza login)
 $app->post('/api/v1/auth/register', AuthController::class . ':register');
 $app->post('/api/v1/auth/token', AuthController::class . ':login');
-// I parcheggi si vedono per tutti per mostrarli sulla mappa
 $app->get('/api/v1/parking-lots', ParkingLotController::class . ':listAll');
 
-// ROTTE PROTETTE (serve il token per accedere)
 $app->group('/api/v1', function ($group) {
-    // Informazioni dell'utente loggato
     $group->get('/auth/me', AuthController::class . ':me');
-
-    // Creare e eliminare parcheggi
     $group->post('/parking-lots', ParkingLotController::class . ':create');
     $group->delete('/parking-lots/{id}', ParkingLotController::class . ':delete');
-
-    // Creare prenotazioni e vedere le tue prenotazioni
     $group->post('/reservations', ReservationController::class . ':create');
     $group->get('/reservations/user', ReservationController::class . ':listByUser');
-
-// Controlla il token JWT per tutte le rotte di questo gruppo
 })->add(new JwtMiddleware($config['JWT_SECRET']));
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->addRoutingMiddleware();
+
+$app->addErrorMiddleware(true, true, true);
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 $app->run();
